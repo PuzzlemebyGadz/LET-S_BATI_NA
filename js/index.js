30c4cfaf890d55ff3b602dae97bd33591c213fc8
PuzzlemebyGadz <gador.gp.b.bsmt@gmail.com>
2025-06-16 00:01:51 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something To Tell You :)'.split('')
+const text = 'LOVE BATI NATA :)'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
